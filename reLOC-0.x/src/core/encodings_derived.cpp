fa5fdfe3b98f4658d47e231f3f04e086da930ff8
Pavel Surynek <pavel.surynek@seznam.cz>
2019-03-22 02:39:24 +0100

    New version of reLOC.

--- reLOC-0.x/src/core/encodings_derived.cpp
+++ reLOC-0.x/src/core/encodings_derived.cpp
@@ -8,7 +8,7 @@
 /*                                                                            */
 /*                                                                            */
 /*============================================================================*/
-/* encodings_derived.cpp / 0.20-kruh_050                                      */
+/* encodings_derived.cpp / 0.20-kruh_051                                      */
 /*----------------------------------------------------------------------------*/
 //
 // Multi-robot path-finding encodings derived from the standard MDD encoding.
